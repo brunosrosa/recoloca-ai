15e9a982a31858b1df041eb7bb7af2bf544bfc0b
brunosrosa <santana.bruno@gmail.com>
2025-06-26 01:41:13 -0300

    refactor(core): Decouple services into separate repositories and update paths

--- rag_infra/src/tests/integration/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Módulo de Testes de Integração RAG
-
-Contém testes de integração completos do sistema RAG.
-"""
-
-__version__ = "1.0.0"
-__author__ = "@AgenteM_ArquitetoTI"
